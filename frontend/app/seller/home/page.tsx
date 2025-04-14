681dd36b454055af48a5fc10f013ce30d9f1b083
Muaaz <muaazbayat@gmail.com>
2025-04-14 16:40:01 +0200

    fix for seller auth

---
+++ frontend/app/seller/home/page.tsx
@@ -0,0 +1,9 @@
+function SellersHome (){
+    return (
+        <div>
+            <h1>Sellers Home</h1>
+        </div>
+    )   
+}
+
+export default SellersHome;
---
+++ frontend/app/seller/products/page.tsx
@@ -0,0 +1,10 @@
+function SellersProducts() {
+    return (
+        <div>
+        <h1>Sellers Products</h1>
+        <p>List of products for sellers.</p>
+        </div>
+    );  
+}
+
+export default SellersProducts;
