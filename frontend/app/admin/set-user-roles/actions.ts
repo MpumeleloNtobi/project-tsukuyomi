cdf51727707d28e77a3ea2b02836f1cc25cacd13
Muaaz <muaazbayat@gmail.com>
2025-04-19 19:06:48 +0200

    fixed action to not redirect

--- frontend/app/admin/set-user-roles/actions.ts
+++ frontend/app/admin/set-user-roles/actions.ts
@@ -11,8 +11,6 @@ export async function setRole(formData: FormData): Promise<void> {
       publicMetadata: { role: formData.get('role') },
     })
     console.log({ message: res.publicMetadata })
-    // 2. Redirect back to the dashboard, causing a reload with fresh data
-    redirect('/admin/set-user-roles'); // <-- ADJUST PATH IF NEEDED
   } catch (err) {
     throw new Error(err instanceof Error ? err.message : String(err))
   }
@@ -26,11 +24,6 @@ export async function removeRole(formData: FormData): Promise<void> {
       publicMetadata: { role: null },
     })
     console.log({ message: res.publicMetadata })
-    // 1. Revalidate the data cache for the dashboard path
-    revalidatePath('/admin/set-user-roles'); // <-- ADJUST PATH IF NEEDED
-
-    // 2. Redirect back to the dashboard, causing a reload with fresh data
-    redirect('/admin/set-user-roles'); // <-- ADJUST PATH IF NEEDED
   } catch (err) {
     throw new Error(err instanceof Error ? err.message : String(err))
   }
