99a23c3c8f727a4e1707678397dd1920dddf3004
Mpumelelo Ntobi <ntobimpumelelo@gmail.com>
2025-05-01 01:42:37 +0200

    Add seller root layout

--- frontend/app/seller/layout.tsx
+++ frontend/app/seller/layout.tsx
@@ -1,10 +1,16 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SellerSidebar } from "@/components/SellerSidebar";
 import Header from "@/components/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
-      <Header/>
-      {children}
-    </main>
+    <SidebarProvider>
+      <SellerSidebar />
+      <main className="w-full">
+        <Header />
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
   )
 }
