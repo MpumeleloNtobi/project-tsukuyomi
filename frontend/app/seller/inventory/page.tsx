65e26f3484fea8f30f137c0a44d3be54a9071f6c
Mpumelelo Ntobi <ntobimpumelelo@gmail.com>
2025-05-01 01:45:35 +0200

    Add seller routes and a seller store page

---
+++ frontend/app/seller/inventory/page.tsx
@@ -0,0 +1,9 @@
+function SellerInventory() {
+    return (
+        <section className="w-full p-8 py-4 space-y-16">
+            <h1>Inventory component goes here</h1>
+        </section>
+    )   
+}
+
+export default SellerInventory;
---
+++ frontend/app/seller/store/page.tsx
@@ -0,0 +1,9 @@
+function SellerStore() {
+    return (
+        <section className="w-full p-8 py-4 space-y-16">
+            <h1>Store component goes here</h1>
+        </section>
+    )   
+}
+
+export default SellerStore;
