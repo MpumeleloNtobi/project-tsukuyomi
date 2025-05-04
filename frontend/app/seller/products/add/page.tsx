01f00d1587d6c750869f9f0dea9e9ca1ac0244dd
Phutheho Mtloung <Mtloungphutheho@gmail.com>
2025-05-05 01:18:18 +0200

    added feature to delete and add product in the products datatable

--- frontend/app/seller/products/add/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import ProductForm from "@/components/forms/addproductform"
-
-function AddProduct(){
-  return(<>
-  <ProductForm></ProductForm>;
-  </>)
-}
-export default AddProduct;
