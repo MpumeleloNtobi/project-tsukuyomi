c6b8daea54ac506b522d0ec8f4378b0fa8e59570
Muaaz <muaazbayat@gmail.com>
2025-05-05 13:07:00 +0200

    cleanup

--- frontend/__tests__/Home.test.js
+++ frontend/__tests__/Home.test.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from '../app/page'; // Adjust the path if necessary
-
 describe('Home Component', () => {
   it('should always pass', () => {
     expect(true).toBe(true);
